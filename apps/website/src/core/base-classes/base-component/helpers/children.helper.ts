3e6b0cfe3a589425537b65ba615dee96c9b64753
Lois KOUNINEF <lkouninef@gmail.com>
2026-05-17 04:52:42 +0200

    _

--- apps/website/src/core/base-classes/base-component/helpers/children.helper.ts
+++ apps/website/src/core/base-classes/base-component/helpers/children.helper.ts
@@ -29,23 +29,6 @@ export class ChildrenHelper {
     });
   }
 
-  // public static addChildren(
-  //   component: BaseComponent, 
-  //   element: HTMLElement, 
-  //   children: BaseComponent[]
-  // ): void {
-  //   const configs: ComponentConfig[] = component.childConfigs();
-  //   configs.forEach(config => {
-  //     element.querySelectorAll(`[data-component="${config.selector}"]`).forEach(el => {
-  //       if (el instanceof HTMLElement) {
-  //         const childComponent = config.factory(el);
-  //         childComponent.render();
-  //         this.registerChild(childComponent, children);
-  //       }
-  //     });
-  //   });
-  // }
-
   public static destroyChildren(children: BaseComponent[]): void {
     children.forEach(child => child.destroy());
   }
