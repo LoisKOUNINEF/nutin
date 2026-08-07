544ce84904f8910fa65736d466134f8510da405b
Lois Kouninef <lkouninef@gmail.com>
2026-08-07 14:41:29 +0200

    _

--- apps/website/src/app/dom.test.js
+++ apps/website/src/app/dom.test.js
@@ -4,10 +4,10 @@ describe('App container', () => {
     expect(appContainer).toBeDefined();
   });
   it('should have title', () => {
-    expect(document.title).toEqual("nutin-website");
+    expect(document.title).toEqual("nutin website");
   });
   it('should have meta description', () => {
     const desc = document.querySelector('meta[name="description"]').content;
-    expect(desc).toEqual('nutin-website');
+    expect(desc).toEqual('nutin website');
   });
 });
--- apps/website/src/core/base-classes/base-component/helpers/dom.helper.ts
+++ apps/website/src/core/base-classes/base-component/helpers/dom.helper.ts
@@ -59,7 +59,7 @@ export class DomHelper {
     });
   }
 
-  private static appendOrReplace(config: DomElementConfig) {
+  private static appendOrReplace(config: DomElementConfig): void {
     if (config.target instanceof HTMLElement) {
       if (typeof config.mountTarget === 'string') {
         // Append mode
@@ -69,7 +69,7 @@ export class DomHelper {
         config.target.replaceWith(config.element);
       }
     } else {
-      console.warn('Mount target not found:', config.mountTarget);
+      return;
     }
   }
 }
