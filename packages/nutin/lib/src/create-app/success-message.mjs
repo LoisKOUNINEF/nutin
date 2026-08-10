3470d263b0920dbbe29249122e8b3f0f9b6fdfa8
Lois Kouninef <lkouninef@gmail.com>
2026-08-10 15:31:51 +0200

    builder minor

--- packages/nutin/lib/src/create-app/success-message.mjs
+++ packages/nutin/lib/src/create-app/success-message.mjs
@@ -4,7 +4,7 @@ import { PACKAGE_HOMEPAGE } from '../common/package-data.mjs';
 export function displaySuccessMessage(answers) {
   const { projectName, packageManager, deployHelper } = answers;
   print.boldSuccess('\n🎉 Your project is ready!');
-  print.boldInfo(`Documentation on ${PACKAGE_HOMEPAGE}`);
+  print.boldInfo(`Documentation on https://${PACKAGE_HOMEPAGE}`);
 
   if (packageManager === 'pnpm' || packageManager === 'bun') {
     if (deployHelper) print.boldError(`You'll need to modify the Dockerfile to support ${packageManager}`)
