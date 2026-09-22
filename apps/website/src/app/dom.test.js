ba19301336382eae0b005386977401ef8eaea2b5
Lois Kouninef <lkouninef@gmail.com>
2026-09-22 14:35:16 +0200

    _

--- apps/website/src/app/dom.test.js
+++ apps/website/src/app/dom.test.js
@@ -3,11 +3,4 @@ describe('App container', () => {
     const appContainer = document.getElementById('app');
     expect(appContainer).toBeDefined();
   });
-  it('should have title', () => {
-    expect(document.title).toEqual("nutin website");
-  });
-  it('should have meta description', () => {
-    const desc = document.querySelector('meta[name="description"]').content;
-    expect(desc).toEqual('nutin website');
-  });
 });
--- apps/website/src/app/views/home/home.view.test.js
+++ apps/website/src/app/views/home/home.view.test.js
@@ -10,9 +10,4 @@ describe('HomeView', () => {
 	it('should be defined', () => {
 		expect(view).toBeDefined();
 	});
-	it('should have title with i18n key', () => {
-		const h1 = view.element.querySelector('h1');
-		expect(h1).toBeDefined();
-		expect(h1.textContent).toBe('home.title');		
-	});
 })
